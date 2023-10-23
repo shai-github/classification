779ed70c1160bc0d32bc19dcadb2b0b21b3f6560
shai-github <shaigithub@gmail.com>
2023-10-22 20:55:05 -0500

    data splits and weights

--- hate_speech/prepare.py
+++ hate_speech/prepare.py
@@ -1,13 +1,9 @@
 import pandas as pd
+import numpy as np
 
 from utils.clean import clean_text
 from utils.embed import embed_text
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import RandomOverSampler
-
-
-SCALER = StandardScaler()
-ROS = RandomOverSampler()
+from sklearn.model_selection import train_test_split
 
 
 def prepare_dataframe() -> pd.DataFrame:
@@ -31,15 +27,50 @@ def prepare_dataframe() -> pd.DataFrame:
     return df
 
 
-def weight_data():
+def weight_data(df: pd.DataFrame) -> dict:
     """
+    Returns weights for each class in the dataframe based on the
+        number of samples in each class - this is done to account
+        for the imbalance in the dataset
+    :param df: dataframe with labels
+    :return: dictionary with weights for each class
+    """
+    n_count, o_count, h_count = np.bincount(df['class'])
+    total = n_count + o_count + h_count
+
+    return {
+        i: (1 / count_type) * (total) / 3.0 
+        for i, count_type in enumerate([n_count, o_count, h_count])
+    }
+
+
+def split_data(df: pd.DataFrame, test_size: float = 0.2) -> dict:
+    """
+    Splits the dataframe into train and test sets
+    :param df: dataframe with tweets and labels
+    :param test_size: size of the test set
+    :return: dictionary with train and test sets
+    """
+    X_train, X_test, y_train, y_test = train_test_split(
+        df['embeddings'], 
+        df['class'], 
+        test_size=test_size, 
+        random_state=42
+    )
     
-    """
-    pass
+    return {'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test}
 
 
 def clean_and_embed(df: pd.DataFrame):
     """
+    Cleans text and embeds it using the pretrained model
+    :param df: dataframe with tweets
+    :return: dataframe with cleaned tweets and embeddings
     """
-    df['clean_tweet'] = df['tweet'].apply(clean_text)
-    df['embeddings'] = df['clean_tweet'].apply(embed_text)
+    # create a deep copy of the dataframe
+    embed_df = df.head().copy()
+
+    embed_df['clean_tweet'] = embed_df['tweet'].apply(clean_text)
+    embed_df['embeddings'] = embed_df['clean_tweet'].apply(embed_text)
+
+    return embed_df
