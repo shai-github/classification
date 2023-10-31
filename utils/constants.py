04db2e0a3b7e0ef20b1ad26836f53a73634f12c1
shai-github <shaigithub@gmail.com>
2023-10-30 19:37:09 -0500

    all constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,10 @@
-FOR_REMOVAL = [
+from nltk.corpus import stopwords
+
+
+TWITTER_SPECIFIC = [
     "rt", "yoooooouuuu", "yoooo", "yooo", "loool", "lmfaoooo", "yoooooooooooooo", "lmfaoooooooo", "squaaaaad", "yoooo", "yoooou",
     "ah", ""
 ]
+
+TOTAL_WORDS = list(set(stopwords.words('english') + TWITTER_SPECIFIC))
+FOR_REMOVAL = [word for word in TOTAL_WORDS if word != "not"]
